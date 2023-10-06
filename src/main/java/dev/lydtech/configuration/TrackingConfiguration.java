ba991fce9527ab32c81a966dae3b6f9ef45783bc
PierreSQS <nomo_messina@web.de>
2023-10-06 15:11:30 +0200

    Sec6_Assn1-Tracking Service-NOK: corrected issue of the previous issue (5714af5f)

--- src/main/java/dev/lydtech/configuration/TrackingConfiguration.java
+++ src/main/java/dev/lydtech/configuration/TrackingConfiguration.java
@@ -4,6 +4,7 @@ import dev.lydtech.dispatch.message.DispatchPreparing;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,8 +44,8 @@ public class TrackingConfiguration {
     public ProducerFactory<String,Object> producerFactory(@Value("${kafka.bootstrap-servers}") String bootStrapServers) {
         Map<String,Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServers);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringDeserializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonDeserializer.class);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
