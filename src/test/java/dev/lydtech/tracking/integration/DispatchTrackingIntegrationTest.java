53774f69d212b6673d644b47d8e77a8617554859
PierreSQS <nomo_messina@web.de>
2023-10-11 01:25:38 +0200

    Sec7_Assn2-Integration Test: step2-setup KafkaTestListener

--- src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
+++ src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
@@ -1,11 +1,19 @@
 package dev.lydtech.tracking.integration;
 
 import dev.lydtech.configuration.TrackingConfiguration;
+import dev.lydtech.dispatch.message.TrackingStatusUpdated;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @SpringBootTest(classes = {TrackingConfiguration.class})
@@ -16,6 +24,27 @@ class DispatchTrackingIntegrationTest {
     private final static String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
 
     private final static String TRACKING_STATUS_TOPIC = "tracking.status";
+
+    @Autowired
+    private KafkaTestListener kafkaTestListener;
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public KafkaTestListener kafkaTestListener() {
+            return new KafkaTestListener();
+        }
+    }
+
+    public static class KafkaTestListener {
+        AtomicInteger trackingStatusCounter = new AtomicInteger(0);
+
+        @KafkaListener(groupId = "KafkaIntegrationTest",topics = {TRACKING_STATUS_TOPIC})
+        void receiveTrackingStatus(@Payload TrackingStatusUpdated trackingStatusUpdated) {
+            log.info("Receive TrackingStatusUpdated Payload: {}",trackingStatusUpdated);
+            trackingStatusCounter.incrementAndGet();
+        }
+    }
 
     @Test
     void testDispatchTrackingFlow() {
