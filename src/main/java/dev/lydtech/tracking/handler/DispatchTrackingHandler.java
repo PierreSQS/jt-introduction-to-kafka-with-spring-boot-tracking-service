08e723aa557e84369435f3eb7f1e373c86d1481d
PierreSQS <nomo_messina@web.de>
2023-10-16 22:31:55 +0200

    Sec10_Chap50-Kafka Handler: modified Consumer to consider multiple events

--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -4,22 +4,25 @@ import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@KafkaListener(
+        id = "dispatchTrackingConsumerClient",
+        topics = "dispatch.tracking",
+        groupId = "tracking.dispatch.tracking.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
 
     private final TrackingService trackingService;
 
-    @KafkaListener(
-            id = "dispatchTrackingConsumerClient",
-            topics = "dispatch.tracking",
-            groupId = "tracking.dispatch.tracking.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+
+    @KafkaHandler
     public void listen (DispatchPreparing payload) {
         log.info("Received Tracking Payload: {}",payload);
 
