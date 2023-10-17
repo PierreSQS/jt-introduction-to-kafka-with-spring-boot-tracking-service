88e8ea37f2518cb6c6865538de65e1482b5139ae
PierreSQS <nomo_messina@web.de>
2023-10-17 14:12:26 +0200

    Sec11_Assn3-Consume Multiple Event Types: step3-extended Consumer Tests to handle new Event

--- src/test/java/dev/lydtech/tracking/handler/DispatchTrackingHandlerTest.java
+++ src/test/java/dev/lydtech/tracking/handler/DispatchTrackingHandlerTest.java
@@ -1,5 +1,6 @@
 package dev.lydtech.tracking.handler;
 
+import dev.lydtech.dispatch.message.DispatchCompleted;
 import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.tracking.service.TrackingService;
 import dev.lydtech.tracking.util.TestEventData;
@@ -13,26 +14,31 @@ import static org.mockito.Mockito.*;
 
 class DispatchTrackingHandlerTest {
 
+    private static final String TRACKING_FAILURE = "Tracking Process failure: ";
+
     TrackingService trackingServMock;
 
     DispatchTrackingHandler dispatchTrackingHandler;
 
     DispatchPreparing dispatchPreparing;
 
+    DispatchCompleted dispatchCompleted;
+
     @BeforeEach
     void setUp() {
         trackingServMock = mock();
         dispatchTrackingHandler = new DispatchTrackingHandler(trackingServMock);
         dispatchPreparing = TestEventData.buildDispatchPreparingEvent(UUID.randomUUID());
+        dispatchCompleted = TestEventData.buildDispatchCompletedEvent(UUID.randomUUID());
     }
 
     @Test
-    void listen_success() throws ExecutionException, InterruptedException {
+    void listen_DispatchPreparing_success() throws ExecutionException, InterruptedException {
         dispatchTrackingHandler.listen(dispatchPreparing);
         verify(trackingServMock).process(dispatchPreparing);
     }
     @Test
-    void listen_throwsException() throws ExecutionException, InterruptedException {
+    void listen_DispatchPreparing_throwsException() throws ExecutionException, InterruptedException {
         // When
         doThrow(RuntimeException.class).when(trackingServMock).process(any(DispatchPreparing.class));
 
@@ -40,4 +46,19 @@ class DispatchTrackingHandlerTest {
         dispatchTrackingHandler.listen(dispatchPreparing);
         verify(trackingServMock).process(dispatchPreparing);
     }
+
+    @Test
+    void listen_DispatchCompleted_success() throws ExecutionException, InterruptedException {
+        dispatchTrackingHandler.listen(dispatchCompleted);
+        verify(trackingServMock).process(dispatchCompleted);
+    }
+
+    @Test
+    void listen_DispatchCompleted_throwsException() throws ExecutionException, InterruptedException {
+        doThrow(new RuntimeException(TRACKING_FAILURE)).when(trackingServMock).process(dispatchCompleted);
+
+        dispatchTrackingHandler.listen(dispatchCompleted);
+        verify(trackingServMock).process(dispatchCompleted);
+    }
+
 }
--- src/test/java/dev/lydtech/tracking/util/TestEventData.java
+++ src/test/java/dev/lydtech/tracking/util/TestEventData.java
@@ -1,7 +1,9 @@
 package dev.lydtech.tracking.util;
 
+import dev.lydtech.dispatch.message.DispatchCompleted;
 import dev.lydtech.dispatch.message.DispatchPreparing;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class TestEventData {
@@ -9,6 +11,13 @@ public class TestEventData {
     public static DispatchPreparing buildDispatchPreparingEvent(UUID orderId) {
         return DispatchPreparing.builder()
                 .orderId(orderId)
+                .build();
+    }
+
+    public static DispatchCompleted buildDispatchCompletedEvent(UUID orderId) {
+        return DispatchCompleted.builder()
+                .orderID(orderId)
+                .date(LocalDate.now().toString())
                 .build();
     }
 
